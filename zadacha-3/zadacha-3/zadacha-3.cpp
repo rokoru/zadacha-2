5e30edaa5e52547d0c595bb07b8be671e2a7b181
Dmitry <gomerty15@mail.ru>
2021-04-19 13:00:52 +0300

    Добавленно решение

---
+++ zadacha-3/zadacha-3/zadacha-3.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    float x;
+    std::cin >> x;
+    if (x < 0) {
+        std::cerr << "error";
+        return 1;
+    }
+    else  if (x < 50) {
+        x = x * 0.3;
+        std::cout << x << " рублей";
+    }
+    else if (50 < x < 75) {
+        x = x * 0.5;
+        std::cout << x << " рублей";
+    }
+    else if (75 < x < 90) {
+        x = x * 0.65;
+        std::cout << x << " рублей";
+    }
+    else {
+        x = (x * 0.7) + 20;
+        std::cout << x << " рублей";
+    }
+    return 0;
+}
